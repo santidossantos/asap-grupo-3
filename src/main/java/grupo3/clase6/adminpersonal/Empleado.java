4b5afd903c6ddf41df1bbeb3ad5f69af83618786
paguero8 <paguero@asap-consulting.net>
2024-07-25 12:08:51 -0300

    clase Empleado

---
+++ src/main/java/grupo3/clase6/adminpersonal/Empleado.java
@@ -0,0 +1,27 @@
+package grupo3.clase6.adminpersonal;
+
+import java.time.LocalDate;
+
+public abstract class Empleado {
+	private String dni;
+	private String nombre;
+	private String apellido;
+	private int ingreso;
+	
+	public Empleado(String dni, String nombre, String apellido, int ingreso) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.ingreso = ingreso;
+	}
+	
+	public abstract double calcularSalario();
+	
+	public int getIngreso() {
+		return this.ingreso;
+	}
+	
+	public int getAntiguedad() {
+		return LocalDate.now().getYear() - getIngreso(); 
+	}
+}
