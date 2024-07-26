34d55c65f1330802dd44b9aa23716ffcdfae5dc5
Nazarena Pantanetti <nazapantanetti@gmail.com>
2024-07-26 12:54:07 -0300

    Clase historial

--- src/main/java/grupo3/clase7/comercioInformatico/Historial.java
+++ src/main/java/grupo3/clase7/comercioInformatico/Historial.java
@@ -1,10 +1,24 @@
 package grupo3.clase7.comercioInformatico;
+
 import java.util.List;
 
 public class Historial {
-	private List <Facturable> facturas;
-	
-	public Historial (List <Facturable> facturas) {
+	private List<Facturable> facturas;
+
+	public Historial(List<Facturable> facturas) {
 		this.facturas = facturas;
+	}
+
+	public List<Facturable> getFacturas() {
+		return facturas;
+	}
+
+	public double montoTotalFacturado() {
+		return facturas.stream().mapToDouble(facturable -> facturable.calcularPrecio()).sum();
+	}
+
+	public int cantServiciosSimples() {
+		return (int) facturas.stream().filter(facturable -> (facturable instanceof ServicioDeReparacion))
+				.map(facturable -> (((ServicioDeReparacion) facturable).getNivelDificultad() < 2)).count();
 	}
 }
